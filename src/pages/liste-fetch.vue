213c4711be60395b6ded7ee3567c41c6234a2983
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-09-23 13:44:05 +0200

    Modification

--- src/pages/liste-fetch.vue
+++ src/pages/liste-fetch.vue
@@ -1,13 +1,13 @@
 <template>
     <div v-for="maison in maisons" :key="maison.nom">
-        <card class="w-1/2 p-5" v-bind="maison" />
+        <card class="w-1/3 p-5" v-bind="maison" />
     </div>
 </template>
 
 <script setup lang="ts">
 
 import card from "../components/card.vue";
-const res = await fetch("./maisons.json");
+const res = await fetch("/public/maisons.json");
 const maisons = await res.json();
 
 </script>
