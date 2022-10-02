2d246c73ec266f3935a85d9fecb1e57f9b632462
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-10-02 14:21:07 +0200

    Edition

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
   import { supabase } from "../../supabase";
+  import groupBy from "lodash/groupBy";
+  import { 
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+   } from '@headlessui/vue'
+
   const { data, error } = await supabase.from("quartiercommune").select("*");
   if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
   </script>
@@ -14,4 +21,24 @@
         </li>
       </ul>
     </section>
+
+    <Disclosure
+    v-for="(liste_Quartier, libelle_Commune) in groupBy(
+      data,
+      'libelle_Commune'
+    )"
+    :key="libelle_Commune"
+  >
+
+    <DisclosureButton>{{libelle_Commune}}</DisclosureButton>
+    <DisclosurePanel>
+      <ul>
+        <li v-for="quartierObject in liste_Quartier" :key="quartierObject.code_Quartier">
+          <RouterLink :to="{ name: 'quartier-id', params: { id: quartierObject.code_Quartier }, }"
+>{{ quartierObject.libelle_Quartier }}</RouterLink>
+        </li>
+      </ul>
+    </DisclosurePanel>
+
+    </Disclosure>
   </template>
